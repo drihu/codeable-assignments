b2a26ce7367b44905f2ed125ee681741e508bd12
Ricardo Huamani <ricardohuamanip@gmail.com>
2020-03-25 17:48:22 -0500

    add mario-more.c

---
+++ module1/week2/day3/mario-more.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int getPositive(void);
+void printPyramid(int height);
+void printChar(char c, int n);
+
+int main(void) {
+    int height = getPositive();
+    printPyramid(height);
+}
+
+int getPositive(void) {
+    int h;
+    do {
+        h = get_int("Height: ");
+    } while (h < 1 || h > 8);
+    return h;
+}
+
+void printPyramid(int height) {
+    for (int i = 1; i <= height; i++) {
+        printChar(' ', height - i);
+        printChar('#', i);
+        printf("  ");
+        printChar('#', i);
+        printf("\n");
+    }
+}
+
+void printChar(char c, int n) {
+    for (int i = 0; i < n; i++) {
+        printf("%c", c);
+    }
+}
