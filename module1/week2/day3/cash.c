46854e748751c6c71c8dbc2b012ab70cabb649c7
Ricardo Huamani <ricardohuamanip@gmail.com>
2020-03-25 18:40:30 -0500

    add cash.c

---
+++ module1/week2/day3/cash.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <math.h>
+#include <cs50.h>
+
+float getPositive(void);
+int drawer(int cents);
+
+int main(void) {
+    float change = getPositive();
+    int cents = round(change * 100);
+    int coins = drawer(cents);
+    printf("%i\n", coins);
+}
+
+float getPositive(void) {
+    float number;
+    do {
+        number = get_float("How much change?: ");
+    } while (number < 0);
+    return number;
+}
+
+int drawer(cents) {
+    int amount = cents;
+    int coins = 0;
+
+    coins = coins + floor(amount / 25);
+    amount = amount % 25;
+    coins = coins + floor(amount / 10);
+    amount = amount % 10;
+    coins = coins + floor(amount / 5);
+    amount = amount % 5;
+    coins = coins + amount;
+
+    return coins;
+}
