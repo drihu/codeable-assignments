4490fcd47e7d542c1b57408e40a7aa3786572312
Ricardo Huamani <ricardohuamanip@gmail.com>
2020-03-25 16:29:41 -0500

    add C file

--- module1/week2/day3/hello.c
+++ module1/week2/day3/hello.c
@@ -1,11 +1,7 @@
 #include <stdio.h>
 #include <cs50.h>
 
-/**
- * Say hello to the user
- */
-int main(void)
-{
+int main(void) {
     string name = get_string("What's your name?\n");
     printf("Hello, %s\n", name);
 }
---
+++ module1/week2/day3/mario-less.c
@@ -0,0 +1,40 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int getPositive(void);
+void printPyramid(int height);
+void printSpace(int n);
+void printHash(int n);
+
+int main(void) {
+    int height = getPositive();
+    printPyramid(height);
+}
+
+int getPositive(void) {
+    int h;
+    do {
+        h = get_int("Height: ");
+    } while (h < 1 || h > 8);
+    return h;
+}
+
+void printPyramid(int height) {
+    for (int i = 1; i <= height; i++) {
+        printSpace(height - i);
+        printHash(i);
+        printf("\n");
+    }
+}
+
+void printSpace(int n) {
+    for (int i = 0; i < n; i++) {
+        printf(" ");
+    }
+}
+
+void printHash(int n) {
+    for (int i = 0; i < n; i++) {
+        printf("#");
+    }
+}
